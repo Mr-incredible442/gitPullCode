c595fa9ac593374ee94de214d21f39d45c7f9ad6
zacharia <zachariayussuf2014@gmail.com>
2024-09-20 12:52:18 +0000

    changed

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const runCommand = (command, options) => {
 // Endpoint to pull from the first repository
 app.get('/pull-server-repo', async (req, res) => {
   try {
-    const stdout = await runCommand('git -C "C:\\Users\\THE GAME\\OneDrive\\Desktop\\yussman-server" pull', { windowsHide: true });
+    const stdout = await runCommand('git -C "/home/userver/yussman-server" pull', { windowsHide: true });
     res.send(`Server Repo Pull Successful: ${stdout}`);
   } catch ({ error, stderr }) {
     console.error(`Error pulling server repo: ${error}`);
@@ -33,19 +33,15 @@ app.get('/pull-server-repo', async (req, res) => {
 // Endpoint to pull from the second repository
 app.get('/pull-client-repo', async (req, res) => {
   try {
-    // Pull the latest changes from the client repository
-    const pullStdout = await runCommand('git -C "C:\\Users\\THE GAME\\OneDrive\\Desktop\\yussman-server\\yussman-client" pull', { windowsHide: true });
+    const pullStdout = await runCommand('git -C "/home/userver/yussman-server/yussman-client" pull', { windowsHide: true });
 
-    // Check if the pull output indicates "Already up to date"
     if (pullStdout.includes('Already up to date')) {
       return res.send('Client Repo is already up to date. Skipping further steps.');
     }
 
-    // Install dependencies and build the client if changes were pulled
-    await runCommand('npm install', { cwd: 'C:\\Users\\THE GAME\\OneDrive\\Desktop\\yussman-server\\yussman-client', windowsHide: true });
-    const buildStdout = await runCommand('npm run build', { cwd: 'C:\\Users\\THE GAME\\OneDrive\\Desktop\\yussman-server\\yussman-client', windowsHide: true });
+    await runCommand('npm install', { cwd: '/home/userver/yussman-server/yussman-client', windowsHide: true });
+    const buildStdout = await runCommand('npm run build', { cwd: '/home/userver/yussman-server/yussman-client', windowsHide: true });
 
-    // Restart PM2 process for the client
     await runCommand('pm2 restart Yussman', { windowsHide: true });
 
     res.send(`Client Repo Pull and Build Successful:\n${pullStdout}\n${buildStdout}`);
@@ -60,4 +56,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(process.env.PORT, process.env.IP || '0.0.0.0', () => {});
+app.listen(process.env.PORT, process.env.IP || '0.0.0.0', () => {
+  console.log(`Server running on ${process.env.IP || '0.0.0.0'}:${process.env.PORT}`);
+});
